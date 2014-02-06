5b1ec76964c09aaee13368913641036da5b9d120
Rob Vermeer <rob@vermeertech.nl>
2014-02-06 02:17:01 +0100

    Add header and footer

---
+++ header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon.ico">
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class("no-js"); ?>>
