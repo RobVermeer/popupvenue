6eba72371e75949ca342c14109bf399951e8a678
Rob Vermeer <rob@vermeertech.nl>
2014-03-16 21:25:40 +0100

    Update footer

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
+	<footer class="clearfix">
+		<div class="container">
+			<div class="row">
+				<?php wp_nav_menu(array('theme_location' => 'footer', 'container' => 'nav', 'container_class' => 'col-md-12', 'menu_class' => 'nav nav-footer', 'fallback_cb' => '')); ?>
+			</div>
+		</div>
+		<a href="#logo" class="top"><?php _e("Top", "puv"); ?></a>
+	</footer>
+	
+	<section class="copyright">
+		&copy; <?php echo date('Y'); ?> <?php _e("All rights reserved", "puv"); ?>. <a href="<?php bloginfo("url"); ?>"><?php bloginfo("name"); ?></a>
+	</section>
 	
 	<?php wp_footer(); ?>
 
