cde21e853b713aed52e27be1fae69b1da90ecb1f
Rob Vermeer <rob@vermeertech.nl>
2014-03-16 21:26:50 +0100

    Add logged out nav

---
+++ partials/nav-logged-out.php
@@ -0,0 +1,9 @@
+<ul>
+	<li>
+		<a href="/login/"><i class="login-join"></i><?php _e("Login/Join", "puv"); ?></a>
+		<ul>
+			<li><a href="<?php bloginfo("url"); ?>/login/" title="<?php _e("Login", "puv"); ?>" class="login"><?php _e("Login", "puv"); ?></a></li>
+			<li><a href="<?php bloginfo("url"); ?>/register/" title="<?php _e("Join", "puv"); ?>" class="register"><?php _e("Join", "puv"); ?></a></li>
+		</ul>
+	</li>
+</ul>
